12c44ad194379e1d79bfc065f66cfc6b9fdd6300
Lee <1303605700@qq.com>
2023-04-06 22:41:01 +0800

    fix

--- C/main.c
+++ C/main.c
@@ -1,5 +1,9 @@
-﻿
+﻿#include <stdio.h>
+
+#include "pointer.h"
+
 int main()
 {
+    func();
     return 0;
 }
---
+++ C/pointer.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "pointer.h"
+
+void func() {
+	int i = 0;
+	printf("%p\n", &i);
+
+}
