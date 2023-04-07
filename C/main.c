2c793a701ce1834f3bfc62f3b9a781ffe2254338
Lee <1303605700@qq.com>
2023-04-07 23:34:58 +0800

    f

--- C/main.c
+++ C/main.c
@@ -4,6 +4,33 @@
 
 int main()
 {
-    func();
+    addOrSub();
+
+
+
+
+    //int i = 0;
+    //int *p = &i;
+    //printf("%p\n", p);
+    //printf("%d\n", *p);
+    //int arr[10] = { 1,2,3,4,5,6 };
+    //printf("%p\n", &arr);
+    //printf("%d\n", arr[0]);
+    //int* p1 = &arr;
+    //printf("%p\n", p1);
+    //printf("%p\n", &arr[0]);
+    //printf("%d\n", p1[0]);
+    //printf("%p\n", p1);
+    //p1++;
+    //p1++;
+    //printf("%p\n", p1);
+    //printf("%d\n", p1[0]);
+    //int* p2 = &arr[2];
+    //printf("%p\n", p2);
+    //printf("%p\n", &p1[0]);
+
+
+
+    
     return 0;
 }
--- C/pointer.c
+++ C/pointer.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include "pointer.h"
 
-void func() {
-	int i = 0;
-	printf("%p\n", &i);
+void addOrSub() {
+	printf("sizeof(char)= %d\n", sizeof(char));
+	printf("sizeof(int)= %d\n", sizeof(int));
+	char c[5] = {'h','e','l','l','o'};
+	printf("%c\n", c[0]);
+	char* p = c;
+	printf("%p\n", p+1);
+	//*(p + n)µÈ¼ÛÓÚc[n]
+	printf("%c\n", *(p + 4));
+	printf("%c\n", c[4]);
+	p++;
+	printf("%p\n", p);
 
+
+	int i[5] = { 1,2,3,4,5 };
+	int* q = i;
+	printf("%p\n", q);
+	q++;
+	printf("%p\n", q);
 }
